31c242945c924e51ef9a768f82ef3b3e214d88aa
ARCANOS0 <tarek2019marco@gmail.com>
2024-12-28 00:37:12 +0200

    a web project for BATU

---
+++ bootstrap-5.0.2-dist/project home/new pages/pro/p/Untitled-1.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    int x=10;
+    if(x>=2)
+    {
+        cout<<"aaaaaaaaaaaaa";
+    }
+    else
+    {
+                cout<<"nnnnnnnnnnn";
+
+    }
+   /*int x=5;
+    int y=3;
+    cout<<(x>3&&x<10)<<endl;
+        cout<<(x>3||x<4)<<endl;
+            cout<<(!(x<3&&x>10))<<endl;*/ 
+
+
+/*string name="unknown";
+int age=0; 
+double gpa=0; 
+char letter='a'; 
+bool success=true; 
+cout<<"enter your name\n"; cin>>name; 
+cout<<"enter your age\n"; cin>>age; 
+cout<<"enter your gpa\n"; cin>>gpa; 
+cout<<"enter your letter\n"; cin>>letter; 
+cout<<" Are you success\n"; cin>>success;
+cout<<"your name is "<<name<<endl;
+cout<<"your age is "<<age<<endl;
+cout<<"your letter is "<<letter<<endl;
+cout<<"your gpa is "<<gpa<<endl;
+cout<<"if success=1 congratulations else never mind:: "<<success<<endl;*/
+    return 0;
+}
